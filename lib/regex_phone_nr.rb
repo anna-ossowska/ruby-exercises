8144dde17799f665918a462986d9b556ebeab6f0
Anna Ossowska <ossowska.anna1@gmail.com>
2020-11-19 16:35:03 +0100

    Validating phone numbers with regex

--- lib/regex_phone_nr.rb
+++ lib/regex_phone_nr.rb
@@ -3,15 +3,14 @@
   # It is valid when starting with a 0 and containing 10 digits. The second digit cannot be a 0.
   # It is also valid when starting with +33 and containing 11 digits. And the digit following the +33 cannot be a 0.
   # The method should ignore space or dashes between digits.
-
-  # should accept 0665363636
-  # should accept 0165363636
-  # should accept 06 65 36 36 36
-  # should accept 06-65-36-36-36
-  # should accept +33 6 65 36 36 36
-  # should reject 06 65 36 36
-  # should reject 2336653636
-  # should reject +3366536361
+    # should accept 0665363636
+    # should accept 0165363636
+    # should accept 06 65 36 36 36
+    # should accept 06-65-36-36-36
+    # should accept +33 6 65 36 36 36
+    # should reject 06 65 36 36
+    # should reject 2336653636
+    # should reject +3366536361
 
 def phone_number?(phone_number)
   phone_number.match?(/^(0|\+33[\s-]?)[1-9][ -]?(\d{2}[\s-]?){4}/) ? true : false
